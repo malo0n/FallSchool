a3e94537b87ce9b3218bdb8dacae0f069b0776b0
maloon <k0ll0bahob@gmail.com>
2023-10-19 23:14:34 +0300

    commit js2

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
-let number = 2;
-console.log(number);
-const object = {
-    name: 'Ivan',
-    age: 18,
-}
-console.log(object.name);
+let userName = document.querySelector('.name_preview')
+let userSex = document.querySelector('.preview_sex')
+let nameInput = document.querySelector('.name')
+
+
+
+
+
+
+nameInput.addEventListener('change', () => {
+    userName.innerHTML = nameInput.value;
+})
+
+
+let radioInput = document.querySelectorAll('.custom-radio');
+radioInput.forEach(element => {
+    addEventListener('change', () => {
+        if (r.checked && radio.value=="Парень") {
+            userSex.innerHTML = "Парень";
+        }
+})})
+
